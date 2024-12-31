578c9b6a4fa343fcc61a879bba914fac1d9e9f5b
adsonluis1 <dev.adsonluis@gmail.com>
2024-12-31 13:01:05 -0300

    adicionando bcrypt e o .env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import {app as routeDevedores} from "./routes/devedores.js"
 import {app as routeDivida} from "./routes/divida.js"
 import {app as routePagamento} from './routes/pagamento.js'
+import {app as routeUsuarios} from './routes/usuarios.js'
+import "dotenv/config"
 import { connect } from "./db/conn.js"
 
 const app = express()
@@ -10,6 +12,7 @@ app.use(express.json())
 app.use('/devedores',routeDevedores)
 app.use('/divida',routeDivida)
 app.use('/pagamento', routePagamento)
+app.use('/usuarios', routeUsuarios)
 
 app.listen('3333',()=>{
     try {
