d425521bd2a9643f9424b3e7d31698c6c54ff656
adsonluis1 <dev.adsonluis@gmail.com>
2024-12-31 12:59:48 -0300

    add uso do midleware que checa se o JWT está autenticado

--- routes/devedores.js
+++ routes/devedores.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import {checkJWT} from "../midlewares/midlewares.js"
 import {DevedoresControllers as Controllers} from "../controllers/devedores.js"
 export const app = express.Router()
 
-app.get('/', Controllers.getDevedores)
-app.get('/divida/:devedorId', Controllers.getDevedoresDividas)
-app.get('/pagamento/:devedorId', Controllers.getDevedoresPagamentos)
-app.post('/save', Controllers.saveDevedor)
-app.patch('/editar', Controllers.changeInfoDevedores)
-app.put('/desativar/:devedorId' , Controllers.deleteDevedor)
-app.put('/ativar/:devedorId' , Controllers.ativarDevedor)
+app.get('/', checkJWT,Controllers.getDevedores)
+app.get('/divida/:devedorId', checkJWT,Controllers.getDevedoresDividas)
+app.get('/pagamento/:devedorId',checkJWT, Controllers.getDevedoresPagamentos)
+app.post('/save',checkJWT, Controllers.saveDevedor)
+app.patch('/editar',checkJWT, Controllers.changeInfoDevedores)
+app.put('/desativar/:devedorId' ,checkJWT, Controllers.deleteDevedor)
+app.put('/ativar/:devedorId' ,checkJWT, Controllers.ativarDevedor)
